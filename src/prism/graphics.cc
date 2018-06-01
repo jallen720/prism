ad04de3e7253c7cacfc58e7c01c897bbb01992bf
Joel Allen <joelallen720@gmail.com>
2018-05-31 22:05:52 -0700

    Clean graphics module initialization.

--- src/prism/graphics.cc
+++ src/prism/graphics.cc
@@ -1,6 +1,7 @@
 /*
 
 TODO:
+    prop validations
     setup debug callbacks
 
 */
@@ -24,21 +25,29 @@ namespace prism
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #ifdef PRISM_DEBUG
-#define LOG_INSTANCE_PROP_NAMES(PROP_TYPE, PROP_COUNT, PROP_ARRAY, PROP_NAME_FIELD) \
-    util_log(nullptr, "available " #PROP_TYPE " names:");                           \
-                                                                                    \
-    if(PROP_COUNT == 0)                                                             \
-    {                                                                               \
-        fprintf(stdout, " none\n");                                                 \
-    }                                                                               \
-    else                                                                            \
-    {                                                                               \
-        fprintf(stdout, "\n");                                                      \
-                                                                                    \
-        for(uint32_t i = 0; i < PROP_COUNT; i++)                                    \
-        {                                                                           \
-            util_log(nullptr, "    %s\n", PROP_ARRAY[i].PROP_NAME_FIELD);           \
-        }                                                                           \
+#define LOG_AVAILABLE_PROP_NAMES(PROP) \
+    util_log(nullptr, "available " #PROP " names (%i):", available_ ## PROP ## _count); \
+ \
+    if(available_ ## PROP ## _count == 0) \
+    { \
+        fprintf(stdout, " none\n"); \
+    } \
+    else \
+    { \
+        fprintf(stdout, "\n"); \
+ \
+        for(uint32_t i = 0; i < available_ ## PROP ## _count; i++) \
+        { \
+            util_log(nullptr, "    %s\n", available_ ## PROP ## _props[i].PROP ## Name); \
+        } \
+    }
+
+#define LOG_REQUESTED_PROP_NAMES(PROP) \
+    util_log(nullptr, "requested " #PROP " names (%i):\n", requested_ ## PROP ## _count); \
+ \
+    for(size_t i = 0; i < requested_ ## PROP ## _count; i++) \
+    { \
+        util_log(nullptr, "    %s\n", requested_ ## PROP ## _names[i]); \
     }
 #endif
 
@@ -64,18 +73,35 @@ namespace prism
 // Interface
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void gfx_init(const char ** extension_names, uint32_t extension_count)
+void gfx_init(const char ** requested_extension_names, uint32_t requested_extension_count)
 {
+    static const size_t MAX_EXTENSION_COUNT = 16;
+    static const size_t MAX_LAYER_COUNT = 16;
+    const char ** requested_layer_names;
+    uint32_t requested_layer_count = 0;
+
+    // Check available extensions.
+    uint32_t available_extension_count = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &available_extension_count, nullptr);
+    VkExtensionProperties available_extension_props[MAX_EXTENSION_COUNT];
+    vkEnumerateInstanceExtensionProperties(nullptr, &available_extension_count, available_extension_props);
+
+    // Check available layers.
+    uint32_t available_layer_count = 0;
+    vkEnumerateInstanceLayerProperties(&available_layer_count, nullptr);
+    VkLayerProperties available_layer_props[MAX_LAYER_COUNT];
+    vkEnumerateInstanceLayerProperties(&available_layer_count, available_layer_props);
+
 #ifdef PRISM_DEBUG
+    // Concatenate requested and debug extension names.
     static const char * DEBUG_EXTENSION_NAMES[]
     {
         VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
     };
 
     static const size_t DEBUG_EXTENSION_COUNT = sizeof(DEBUG_EXTENSION_NAMES) / sizeof(void *);
-    static const size_t MAX_EXTENSION_COUNT = 16;
     const char * all_extension_names[MAX_EXTENSION_COUNT];
-    const size_t all_extension_count = DEBUG_EXTENSION_COUNT + extension_count;
+    const size_t all_extension_count = DEBUG_EXTENSION_COUNT + requested_extension_count;
 
     if(all_extension_count > MAX_EXTENSION_COUNT)
     {
@@ -87,44 +113,40 @@ void gfx_init(const char ** extension_names, uint32_t extension_count)
             "    debug extensions: %i\n",
             all_extension_count,
             MAX_EXTENSION_COUNT,
-            extension_count,
+            requested_extension_count,
             DEBUG_EXTENSION_COUNT);
     }
 
-    mem_concat(extension_names, DEBUG_EXTENSION_NAMES, extension_count, DEBUG_EXTENSION_COUNT, all_extension_names);
-    extension_names = all_extension_names;
-    extension_count = all_extension_count;
+    mem_concat(
+        requested_extension_names,
+        DEBUG_EXTENSION_NAMES,
+        requested_extension_count,
+        DEBUG_EXTENSION_COUNT,
+        all_extension_names);
 
-    // Log requested extensions.
-    util_log(nullptr, "requested extension names:\n");
+    requested_extension_names = all_extension_names;
+    requested_extension_count = all_extension_count;
 
-    for(size_t i = 0; i < extension_count; i++)
+    // Concatenate requested and debug layer names.
+    static const char * DEBUG_LAYER_NAMES[] =
     {
-        util_log(nullptr, "    %s\n", extension_names[i]);
-    }
+        "VK_LAYER_LUNARG_standard_validation",
+    };
 
-    // Check available extensions.
-    uint32_t available_extension_count = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &available_extension_count, nullptr);
+    static const size_t DEBUG_LAYER_COUNT = sizeof(DEBUG_LAYER_NAMES) / sizeof(void *);
+    requested_layer_names = DEBUG_LAYER_NAMES;
+    requested_layer_count = DEBUG_LAYER_COUNT;
 
-    VkExtensionProperties * available_extension_names =
-        (VkExtensionProperties *)malloc(sizeof(VkExtensionProperties) * available_extension_count);
-
-    vkEnumerateInstanceExtensionProperties(nullptr, &available_extension_count, available_extension_names);
-    LOG_INSTANCE_PROP_NAMES(extension, available_extension_count, available_extension_names, extensionName)
-    free(available_extension_names);
-
-    // Check available layers.
-    uint32_t available_layer_count = 0;
-    vkEnumerateInstanceLayerProperties(&available_layer_count, nullptr);
-
-    VkLayerProperties * available_layers =
-        (VkLayerProperties *)malloc(sizeof(VkLayerProperties) * available_layer_count);
-
-    vkEnumerateInstanceLayerProperties(&available_layer_count, available_layers);
-    LOG_INSTANCE_PROP_NAMES(layer, available_layer_count, available_layers, layerName)
-    free(available_layers);
+    // Log requested and available extensions and layers.
+    LOG_REQUESTED_PROP_NAMES(extension)
+    LOG_AVAILABLE_PROP_NAMES(extension)
+    LOG_REQUESTED_PROP_NAMES(layer)
+    LOG_AVAILABLE_PROP_NAMES(layer)
 #endif
+
+    // // Validate requested extensions and layers are available.
+    // VALIDATE_REQUESTED_PROP_NAMES(extension)
+    // VALIDATE_REQUESTED_PROP_NAMES(layer)
 
     // Initialize application info.
     VkApplicationInfo app_info = {};
@@ -139,20 +161,10 @@ void gfx_init(const char ** extension_names, uint32_t extension_count)
     VkInstanceCreateInfo instance_create_info = {};
     instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     instance_create_info.pApplicationInfo = &app_info;
-    instance_create_info.enabledExtensionCount = extension_count;
-    instance_create_info.ppEnabledExtensionNames = extension_names;
-
-#if PRISM_DEBUG
-    static const char * VALIDATION_LAYERS[] =
-    {
-        "VK_LAYER_LUNARG_standard_validation",
-    };
-
-    instance_create_info.enabledLayerCount = sizeof(VALIDATION_LAYERS) / sizeof(void *);
-    instance_create_info.ppEnabledLayerNames = VALIDATION_LAYERS;
-#else
-    instance_create_info.enabledLayerCount = 0;
-#endif
+    instance_create_info.enabledExtensionCount = requested_extension_count;
+    instance_create_info.ppEnabledExtensionNames = requested_extension_names;
+    instance_create_info.enabledLayerCount = requested_layer_count;
+    instance_create_info.ppEnabledLayerNames = requested_layer_names;
 
     // Create Vulkan instance.
     VkInstance instance;
